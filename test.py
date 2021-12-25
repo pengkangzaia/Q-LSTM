63a284c82caa2017a183039f35cc59cac422d0e4
camille <pengkangzaia@foxmail.com>
2021-12-25 22:29:19 +0800

    测试

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-from psm_main import *
+import torch.cuda
 
-# train_psm()
-test_psm()
+# from psm_main import *
+# test_psm(nrows=87841)
+from swat_main import *
+test_swat(nrows=449919)
+
+# swat test 449919 train 495000
+# 87841 test
+# df = pd.read_csv('data/psm/test.csv')
+# shape = df.shape
+# pass
+# a = torch.cuda.is_available()
+
+
+# from smd_main import *
+
+# train_smd(nrows=1000)
+# test_smd_for_all_entity(nrows=None)
+
+
+
+pass
